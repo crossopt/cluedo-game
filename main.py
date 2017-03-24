98679464bbc2c66f6f1b0b71880855782b099dae
GreatKorn <anton.kvasha04052000@gmail.com>
2017-03-24 19:01:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import telebot
 import logging
 import colorlog
@@ -5,3 +6,12 @@ import emoji
 import time
 import datetime
 
+TOKEN = "303602093:AAGz6ihk895s3K07vYqc6eBY8InFwX4YuhQ"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands = ['hello'])
+def send_msg(message):
+    msg = bot.send_message(message.chat.id, "Darova")
+
+bot.polling()
